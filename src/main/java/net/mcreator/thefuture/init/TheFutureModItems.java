5ebe563be810babf61bef9969467019750d047b4
Patri0101 <Creator@DESKTOP-QNRTKVC>
2022-08-16 08:54:34 +0200

    d

--- src/main/java/net/mcreator/thefuture/init/TheFutureModItems.java
+++ src/main/java/net/mcreator/thefuture/init/TheFutureModItems.java
@@ -14,7 +14,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
-import net.mcreator.thefuture.item.RedbullItem;
 import net.mcreator.thefuture.item.NukeCokeItem;
 import net.mcreator.thefuture.item.HardwareItem;
 import net.mcreator.thefuture.TheFutureMod;
@@ -24,7 +23,6 @@ public class TheFutureModItems {
 	public static final RegistryObject<Item> ROBOT = REGISTRY.register("robot_spawn_egg",
 			() -> new ForgeSpawnEggItem(TheFutureModEntities.ROBOT, -1, -1, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 	public static final RegistryObject<Item> HARDWARE = REGISTRY.register("hardware", () -> new HardwareItem());
-	public static final RegistryObject<Item> REDBULL = REGISTRY.register("redbull", () -> new RedbullItem());
 	public static final RegistryObject<Item> NUKE_COKE = REGISTRY.register("nuke_coke", () -> new NukeCokeItem());
 	public static final RegistryObject<Item> RADIOACTIVE_BLOCK = block(TheFutureModBlocks.RADIOACTIVE_BLOCK, CreativeModeTab.TAB_BUILDING_BLOCKS);
 
