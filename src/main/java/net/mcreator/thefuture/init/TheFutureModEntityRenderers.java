14a7cdffc6bf853ec70620fa6187f83bde7bd87a
Patri0101 <Creator@DESKTOP-QNRTKVC>
2022-08-16 09:15:56 +0200

    ds

--- src/main/java/net/mcreator/thefuture/init/TheFutureModEntityRenderers.java
+++ src/main/java/net/mcreator/thefuture/init/TheFutureModEntityRenderers.java
@@ -9,6 +9,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+
 import net.mcreator.thefuture.client.renderer.RobotRenderer;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -16,5 +18,6 @@ public class TheFutureModEntityRenderers {
 	@SubscribeEvent
 	public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
 		event.registerEntityRenderer(TheFutureModEntities.ROBOT.get(), RobotRenderer::new);
+		event.registerEntityRenderer(TheFutureModEntities.PISTOL.get(), ThrownItemRenderer::new);
 	}
 }
